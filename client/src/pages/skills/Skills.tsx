3d732ca897c4a2245cfef35f2852b852e9446709
goddemi <goddemi0124@gmail.com>
2022-11-03 16:04:15 +0900

    Add : skills 레이아웃

--- client/src/pages/skills/Skills.tsx
+++ client/src/pages/skills/Skills.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+import mac from "./mac.png";
 
 const Skills = () => {
-  return <div></div>;
+  return (
+    <Container>
+      <Mac src={mac}></Mac>
+    </Container>
+  );
 };
 
 export default Skills;
+
+const Container = styled.div`
+  margin-top: 20px;
+`;
+
+const Mac = styled.img``;
+const ReactImg = styled.img`
+  position: absolute;
+`;
