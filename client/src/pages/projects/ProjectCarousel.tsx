8c04858b3c7e9c9da507be460a440daad51bd32a
goddemi <goddemi0124@gmail.com>
2022-11-09 18:21:12 +0900

    Modify : projects imageData 별도파일 분리

--- client/src/pages/projects/ProjectCarousel.tsx
+++ client/src/pages/projects/ProjectCarousel.tsx
@@ -6,38 +6,9 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import Images from "./Images";
+import { IMAGE_DATA } from "./imageData";
 
 const ProjectCarousel = () => {
-  const IMAGE_DATA = [
-    {
-      id: "honey",
-      name: "My Honey Trip",
-      links: [
-        "/reserve/imgs/honeytrip1.png",
-        "/reserve/imgs/honeytrip2.png",
-        "/reserve/imgs/honeytrip4.png",
-      ],
-    },
-    {
-      id: "weggle",
-      name: "Weggle",
-      links: [
-        "/reserve/imgs/weggle1.png",
-        "/reserve/imgs/weggle2.png",
-        "/reserve/imgs/weggle3.png",
-      ],
-    },
-    {
-      id: "turtle",
-      name: "Turtle Home",
-      links: [
-        "/reserve/imgs/ggobuk1.png",
-        "/reserve/imgs/ggobuk2.png",
-        "/reserve/imgs/ggobuk3.png",
-      ],
-    },
-  ];
-
   // 나중에 인덱스가 아닌, id 값에 따라서 백엔드에서 다른 값을 가져올 수 있도록 할 수 있겠다.
 
   const settings = {
---
+++ client/src/pages/projects/imageData.ts
@@ -0,0 +1,29 @@
+export const IMAGE_DATA = [
+  {
+    id: "honey",
+    name: "My Honey Trip",
+    links: [
+      "/reserve/imgs/honeytrip1.png",
+      "/reserve/imgs/honeytrip2.png",
+      "/reserve/imgs/honeytrip4.png",
+    ],
+  },
+  {
+    id: "weggle",
+    name: "Weggle",
+    links: [
+      "/reserve/imgs/weggle1.png",
+      "/reserve/imgs/weggle2.png",
+      "/reserve/imgs/weggle3.png",
+    ],
+  },
+  {
+    id: "turtle",
+    name: "Turtle Home",
+    links: [
+      "/reserve/imgs/ggobuk1.png",
+      "/reserve/imgs/ggobuk2.png",
+      "/reserve/imgs/ggobuk3.png",
+    ],
+  },
+];
