3e0503b3b78f05d587dad184dfec23fa88e0479e
goddemi <goddemi0124@gmail.com>
2022-11-11 15:23:41 +0900

    Add : contact page 이메일 추가

--- client/src/pages/contact/Contact.tsx
+++ client/src/pages/contact/Contact.tsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { BsMailbox } from "react-icons/bs";
 import styled from "styled-components";
 
 const Contact = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <div>
+        <BsMailbox />
+      </div>
+      <a href="mailto:goddemi0124@gmail.com"> goddemi0124@gmail.com</a>
+    </Container>
+  );
 };
 
 export default Contact;
 
-const Container = styled.div``;
+const Container = styled.div`
+  font-size: 50px;
+  font-weight: bold;
+  text-align: center;
+  div {
+    font-size: 100px;
+  }
+`;
+
+const Box = styled.div``;
