81b0f3928a77e50dfcba21c3cd9169be86b9a610
goddemi <goddemi0124@gmail.com>
2022-11-14 20:17:18 +0900

    Modify : cors에러 해결

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -1,8 +1,8 @@
 export const PROJECTS_DATA_URL =
-  "https://myportfolio3039.s3.ap-northeast-2.amazonaws.com/reserve/data/PROJECTS_DATA.json";
+  "https://proxy.cors.sh/myportfolio3039.s3.ap-northeast-2.amazonaws.com/reserve/data/PROJECTS_DATA.json";
 export const SKILLS_DATA_URL =
-  "https://myportfolio3039.s3.ap-northeast-2.amazonaws.com/reserve/data/SKILLS_DATA.json";
+  "https://proxy.cors.sh/myportfolio3039.s3.ap-northeast-2.amazonaws.com/reserve/data/SKILLS_DATA.json";
 export const PUBLIC_URL =
-  "https://myportfolio3039.s3.ap-northeast-2.amazonaws.com/";
+  "https://proxy.cors.sh/myportfolio3039.s3.ap-northeast-2.amazonaws.com/";
 
 //로컬로 실행시에 PUBLIC_URL 자리에 -> 이놈을 넣으면 된다. process.env.PUBLIC_URL
